7cf949ced77cf13e8cccad0877cc003c7b23f8ce
Titus Muthomi <90460893+titusmuthomi@users.noreply.github.com>
2023-02-03 14:29:16 +0300

    firstapp

---
+++ firstapp/android/app/src/main/java/com/tito/flutterfirst/firstapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tito.flutterfirst.firstapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ helloworld/android/app/src/main/java/com/tito/flutter101/hello/helloword/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tito.flutter101.hello.helloword;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
